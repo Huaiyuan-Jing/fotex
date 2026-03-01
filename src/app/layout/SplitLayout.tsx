9d843858bac67c4da27d1d327033e1406ea728c7
HYJing <ls.hyjing@gmail.com>
2026-02-28 21:38:28 -0600

    arrangement minor fix

--- src/app/layout/SplitLayout.tsx
+++ src/app/layout/SplitLayout.tsx
@@ -11,20 +11,37 @@ export function SplitLayout() {
   return (
     <div className="split-layout">
       <AppHeader />
+      
+      {/* 核心主区域，改为左右两半 */}
       <div className="split-main">
-        <aside className="split-sidebar">
-          <FileTree />
-        </aside>
-        <div className="split-editor">
-          <LatexEditor />
+        
+        {/* ================= 左半屏 ================= */}
+        <div className="split-left-workspace">
+          {/* 左侧上半部分：文件树 + 代码编辑器 */}
+          <div className="split-left-top">
+            <aside className="split-sidebar">
+              <FileTree />
+            </aside>
+            <div className="split-editor">
+              <LatexEditor />
+            </div>
+          </div>
+          
+          {/* 左侧下半部分：AI 对话框 (长度仅与左屏等宽) */}
+          <div className="split-assistant-bottom">
+            <AssistantPanel />
+          </div>
         </div>
-        <div className="split-preview">
-          <PdfTabs />
+
+        {/* ================= 右半屏 ================= */}
+        <div className="split-right-workspace">
+          <div className="split-preview">
+            <PdfTabs />
+          </div>
         </div>
+
       </div>
-      <div className="split-assistant-bottom">
-        <AssistantPanel />
-      </div>
+
       <StatusBar />
       <Toast />
     </div>
